cb22c10b0064b3ba1ddfac633dccbf4960f495ca
Deepak Gupta <deepakgupta260820@gmail.com>
2026-01-17 22:58:09 +0530

    initial commit

---
+++ src/test/java/com/example/semanticsearch/SemanticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semanticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
